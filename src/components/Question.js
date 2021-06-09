443d4d78709ffe0abccae4c8e0d029fa7bbd8499
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-06-09 11:58:55 +0200

    Add budget and validate it

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Question = () => {
+  // define state
+  const [amount, setAmount] = useState(0);
+  const [error, setError] = useState(false);
+
+  // function that sets our budget
+  const fixBudget = (ev) => {
+    setAmount(parseInt(ev.target.value));
+  };
+
+  // submit to add the budget
+  const addBudget = (ev) => {
+    ev.preventDefault();
+
+    //   validation
+    if (amount < 1 || isNaN(amount)) {
+      setError(true);
+      return;
+    }
+    //   if validation is ok
+    setError(false);
+  };
+
   return (
     <>
       <h2>What's your budget estimation?</h2>
-      <form>
+
+      <form onSubmit={addBudget}>
         <input
           type="number"
           className="u-full-width"
           placeholder="Write your estimation here"
+          onChange={fixBudget}
         />
         <input
           type="submit"
